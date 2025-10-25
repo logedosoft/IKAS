9b19dfefd6167bc480c55be6a7edf32c048ec560
Beytullah Demşek <beytullah_demsek@hotmail.com>
2025-10-25 16:37:00 +0300

    feat: https://app.asana.com/1/846002128144642/project/1199512727558833/task/1211652317331955 , https://app.asana.com/1/846002128144642/project/1199512727558833/task/1211627149027786 , https://app.asana.com/1/846002128144642/project/1199512727558833/task/1211652317331956 işlemleri yapıldı.

--- ikas/ikas_utils.py
+++ ikas/ikas_utils.py
@@ -8,6 +8,8 @@ from frappe.model.document import Document
 from frappe.utils import cint, flt
 from frappe.utils import now_datetime
 
+from datetime import datetime
+
 
 def process_ikas_auth(store_name, client_id, client_secret):
 
@@ -40,8 +42,7 @@ def get_ikas_order_info(order_id):
         'Content-Type': 'application/json',
         'Authorization': "Bearer " + token
     }
-    print(token)
-    print(order_id)
+
     # GraphQL query, f-string ile order_id yerleştiriliyor
     query = f"""
     query {{
@@ -191,10 +192,8 @@ def get_ikas_order_info(order_id):
       }}
     }}
     """
-    print("*******************DENEME***************************")
     response = requests.post(api_url, json={"query": query}, headers=headers,timeout=3)
-    print(response.status_code)
-    print(response.text)
+
 
     if response.status_code != 200:
         dctResult = {
@@ -207,79 +206,208 @@ def get_ikas_order_info(order_id):
             'order_info': response.json()
         }
 
-    print(dctResult)
+    #print(dctResult)
     return dctResult
 
 
 @frappe.whitelist()
-def process_ikas_order(order_id):
-	
-	#Get token, call get_ikas_order_info with token. Process Order info.
+def process_ikas_order(order_id,doc):
+    """
+    IKAS sipariş bilgilerini alır, gerekli müşteri ve adres dokümanlarını oluşturur
+    ve Sales Order formu için kullanılacak bilgileri JSON olarak döner.
+    """
+    dctResult = {
+        'op_result': False,
+        'op_message': '',
+        'order_info': '',
+        'order': {}
+    }
+    doc = frappe.get_doc(json.loads(doc))
 
-	dctResult = {
-		'op_result': False,
-		'op_message': '',
-		'order_info': ''
-	}
+    # IKAS API'den sipariş bilgisini al
+    dctOrderInfo = get_ikas_order_info(order_id)
 
-	dctOrderInfo = get_ikas_order_info(order_id)
+    if not dctOrderInfo['op_result']:
+        dctResult['op_result'] = False
+        dctResult['op_message'] = dctOrderInfo['op_message']
+        return dctResult
 
-	if dctOrderInfo['op_result'] == False:
-		dctResult['op_result'] = False
-		dctResult['op_message'] = dctOrderInfo['op_message']
-	else:
-		#We Get the order info
-		dctResult['op_result'] = True
-		
-		order = dctOrderInfo['order_info']['data']['listOrder']['data'][0]
-		customer = order.get('customer', {})
+    dctResult['op_result'] = True
+    order_list = dctOrderInfo.get('order_info', {}).get('data', {}).get('listOrder', {}).get('data', [])
+    if not order_list:
+        frappe.throw("Sipariş numarası bulunamadı.")
+    else:
+        order = order_list[0]
 
-		first_name = customer.get('firstName', '')
-		last_name = customer.get('lastName', '')
-		
-		
-		Defaut_Customer = frappe.db.get_single_value('IKAS Settings', 'custumer_name')
-		if	Defaut_Customer:
-			print(Defaut_Customer + "******************DENEME*********************")
-			#Creat address first
-			docAddress = frappe.new_doc('Address')
-			docAddress.address_title = f"{first_name} {last_name}"
-			docAddress.address_type = "Shipping"
-			docAddress.address_line1 = "ASD"
-			docAddress.address_line2 = "TEST"
-			docAddress.city = order.get('shippingAddress', {}).get('city', {}).get('name', '')
-			docAddress.country = "Turkey"
-			docAddress.append("links", {
-				"link_doctype": "Customer", 
-				"link_name": Defaut_Customer})
-			docAddress.save()
-		else:
-			#Create new customer with first and last name
-			docCustomer = frappe.new_doc('Customer')
-			print( "***************************************")
-			docCustomer.customer_name = f"{first_name} {last_name}"
-			#docCustomer.payment_terms = "%50 CASH %50 60 DAYS"
-			docCustomer.customer_type = "Company"
-			docCustomer.customer_group = "Individual"
-			docCustomer.custom_ld_country = "United States"
-			docCustomer.save()
-			docAddress = frappe.new_doc('Address')
-			docAddress.address_title = f"{first_name} {last_name}"
-			docAddress.address_type = "Shipping"
-			docAddress.address_line1 = "ASD"
-			docAddress.address_line2 = "TEST"
-			docAddress.city = order.get('shippingAddress', {}).get('city', {}).get('name', '')
-			docAddress.country = "Turkey"
-			docAddress.append("links", {
-				"link_doctype": "Customer", 
-				"link_name": docCustomer.name})
-			docAddress.save()
-			
+    
+
+    # Daha önce aktarılmış mı kontrol et
+    existing_so = frappe.db.exists("Sales Order", {"po_no": order_id})
+
+    if existing_so:
+        frappe.throw(f"Bu sipariş ({order_id}) daha önce aktarılmış.")
 
 
-		print(f"FN = {first_name}, LN = {last_name}")
+    customer_data = order.get('customer', {})
+    first_name = customer_data.get('firstName', '')
+    last_name = customer_data.get('lastName', '')
 
-	return dctResult
+    default_customer_name = frappe.db.get_single_value('IKAS Settings', 'custumer_name')
+
+    # Adres zaten var mı kontrol et
+    existing_address = frappe.db.exists("Address", {"address_title": f"{first_name} {last_name.strip()}"})
+    # Müşteri ve adres oluşturma
+    if default_customer_name:
+         if not existing_address:
+        # Default müşteri varsa sadece adres oluştur
+            create_address(order, default_customer_name, first_name, last_name)
+            customer_for_order = default_customer_name
+     # Yeni müşteri oluşturulmadan önce adres var mı kontrol et
+    if existing_address:
+        # Adres varsa, bağlı müşteri adını al
+        existing_customer = frappe.db.get_value("Dynamic Link",
+            {"parent": existing_address, "link_doctype": "Customer"}, "link_name"
+        )
+
+        if existing_customer:
+            customer_for_order = existing_customer
+        else:
+            # Adres var ama müşterisi yoksa yeni müşteri oluştur
+            docCustomer = frappe.new_doc('Customer')
+            docCustomer.customer_name = f"{first_name} {last_name.strip()}"
+            docCustomer.customer_type = "Company"
+            docCustomer.customer_group = "Individual"
+            docCustomer.save()
+
+            create_address(order, docCustomer.name, first_name, last_name)
+            customer_for_order = docCustomer.name
+    else:
+        # Adres yoksa tamamen yeni müşteri oluştur
+        docCustomer = frappe.new_doc('Customer')
+        docCustomer.customer_name = f"{first_name} {last_name.strip()}"
+        docCustomer.customer_type = "Company"
+        docCustomer.customer_group = "Individual"
+        docCustomer.save()
+
+
+    doc.customer = customer_for_order
+    doc.customer_name = customer_for_order
+    doc.customer_address = f"{first_name} {last_name.strip()}-Shipping"
+    doc.po_no=order_id
+
+
+    ordered_at = order.get('orderedAt', '')
+    date = ''
+    if ordered_at:
+        # Eğer değer milisaniye cinsindense (örn: 1761204968235)
+        try:
+            date = datetime.fromtimestamp(ordered_at / 1000).strftime('%Y-%m-%d %H:%M:%S')
+        except Exception:
+            date = str(ordered_at)  # Eğer zaten tarih string'iyse, direkt al
+    doc.transaction_date=date
+    doc.delivery_date=date
+    doc.po_date=date
+
+    # Eski satırları temizle
+    if len(doc.get("items", [])) > 0:
+        doc.items = []
+
+    total_qty = 0
+    total_amount = 0
+
+    for line in order.get('orderLineItems', []):
+        variant = line.get('variant', {})
+
+        # variantValues listesi alınıyor
+        variant_values = variant.get('variantValues', [])
+        # Listeyi dolaşarak variantValueName'leri birleştiriyoruz
+        variant_value_names = ', '.join([v.get('variantValueName', '') for v in variant_values])
+        variant_uom = variant_values[0].get('variantTypeName', '') if variant_values else ''
+
+
+        
+        # itemname = ürün adı + variant değerleri
+        itemname = variant.get('name', '')
+        if variant_value_names:
+            itemname += ' ' + variant_value_names
+
+        qty = line.get('quantity', 0)
+        rate = line.get('finalPrice', 0)
+        amount = qty * rate
+
+        # doc.items'e ekleme
+        doc.append('items', {
+            'item_code': itemname,
+            'item_name': itemname,
+            'qty': qty,
+            'rate': rate,
+            'amount': amount,
+            'delivery_date': date,
+            'uom':variant_uom
+        })
+
+        # Toplamları biriktir
+        total_qty += qty
+        total_amount += amount
+
+    shipping_lines = order.get('shippingLines', [])
+    if shipping_lines:
+        for ship in shipping_lines:
+            title = ship.get('title')
+            price = ship.get('price', 0)
+            if title:  # Boş veya None değilse
+                doc.append('items', {
+                    'item_code': 'KARGO',
+                    'item_name': title,
+                    'qty': 1,
+                    'rate': price,
+                    'amount': price,
+                    'delivery_date': date,
+                    'uom':'Adet'
+                })
+                total_qty += 1
+                total_amount += price
+
+    # Doc genel alanlarına toplamları yaz
+    doc.total_qty = total_qty
+    doc.total = total_amount
+    doc.grand_total=total_amount
+    totalFinalPrice=order.get('totalFinalPrice','')
+    total_amountkontrol = sum(float(item.get('amount') or 0) for item in doc.get('items', []))
+
+    if totalFinalPrice != total_amountkontrol:
+        frappe.throw("ERP Sipariş tutarı ile IKAS tutarı tutarsız. Kontrol ediniz.")
+
+
+    dctResult['doc'] = doc
+
+
+    return dctResult
+
+def create_address(order, customer_name, first_name, last_name):
+    """Shipping Address oluşturur ve Customer ile ilişkilendirir"""
+    docAddress = frappe.new_doc('Address')
+    docAddress.address_title = f"{first_name} {last_name}"
+    docAddress.address_type = "Shipping"
+    docAddress.address_line1 = order.get('shippingAddress', {}).get('addressLine1', '')
+    docAddress.state = order.get('shippingAddress', {}).get('district', {}).get('name', '')
+    docAddress.city = order.get('shippingAddress', {}).get('city', {}).get('name', '')
+    country = order.get('shippingAddress', {}).get('country', {}).get('name', '')
+    if country == "Türkiye":
+        country = "Turkey"
+    docAddress.country = country
+    docAddress.email_id = order.get('customer', {}).get('email', '')
+    docAddress.phone = order.get('customer', {}).get('phone', '')
+
+    docAddress.append("links", {
+        "link_doctype": "Customer",
+        "link_name": customer_name
+    })
+    docAddress.save()
+
+
+
+
 
 @frappe.whitelist()
 def get_ikas_auth_token_py():
@@ -296,17 +424,13 @@ def get_ikas_auth_token_py():
         store_name = ikas_settings.store_name
         client_id = ikas_settings.client_id
         client_secret = ikas_settings.client_secret
-		
-		
 
-        
         # process_ikas_auth fonksiyonunu doğrudan çağır
         result = process_ikas_auth(store_name, client_id, client_secret)
-		
+
         # Sonucu kontrol et
         if not result.get("op_result"):
             frappe.throw(result.get("op_message", _("Sunucudan geçerli bir yanıt alınamadı!")))
-			
         # Token ve tarih bilgilerini kaydet
         ikas_settings.token = result.get("auth_token")
         ikas_settings.token_valid_upto = now_datetime()
--- ikas/public/js/sales_order.js
+++ ikas/public/js/sales_order.js
@@ -1,39 +1,40 @@
-/*LOGEDOSOFT@2025
+/* LOGEDOSOFT@2025
 Sales Order IKAS button
 */
 
 function get_ikas_order(frm) {
-	//Ask user order id in frappe dialog then call get_ikas_order_info in ikas_utils.py
-	frappe.prompt({
-		label: __('Order ID'),
-		fieldname: 'order_id',
-		fieldtype: 'Int'
-	}, (values) => {
-		frappe.call({
-			method: 'ikas.ikas_utils.process_ikas_order',
-			args: {
-				order_id: values.order_id
-			},
-			callback: (r) => {
-				console.log(r);
-				if (r.message.op_result == false) {
-					frappe.throw(r.message.op_message);
-				} else {
-
-				}
-			}
-		})
-	});
+    frappe.prompt({
+        label: __('Order ID'),
+        fieldname: 'order_id',
+        fieldtype: 'Int'
+    }, (values) => {
+        frappe.call({
+            method: 'ikas.ikas_utils.process_ikas_order',
+            args: {
+                order_id: values.order_id,
+				doc: frm.doc
+            },
+            callback: (r) => {
+                if (!r.message.op_result) {
+                    frappe.throw(r.message.op_message);
+                } else {
+					frappe.model.sync(r.message.doc);
+					frm.dirty();
+					frm.refresh_fields(["items", "roll_count", "total_weight"]); 
+                    frappe.msgprint(__('Sales Order bilgileri IKAS verisi ile dolduruldu.'));
+                }
+            }
+        })
+    });
 }
 
 frappe.ui.form.on("Sales Order", {
-	refresh(frm) {
-		if (frm.doc.__islocal === 1) {
-			frm.add_custom_button(
-				__("IKAS Sales Order"),
-				() => get_ikas_order(frm)
-				//__("Stock Reservation")
-			);
-		}
-	}
+    refresh(frm) {
+        if (frm.doc.__islocal === 1) {
+            frm.add_custom_button(
+                __("IKAS Sales Order"),
+                () => get_ikas_order(frm)
+            );
+        }
+    }
 });
